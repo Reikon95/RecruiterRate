03503a4be9f5d0d73a36fd321ec8fbd7b89f60e5
Cameron Blackwood <cameronjwblackwood@gmail.com>
2020-03-23 20:31:25 +0000

    landing page

---
+++ src/landingpage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function LandingPage() {
+    return (
+        <>
+        <h1>Say goodbye to smoke and mirrors.</h1>
+        <h1>Say hello to simple hiring!</h1>
+        <h3>RecruiterRate is a simple platform to discover and review recruiters. For too long, the industry has suffered from 
+        cowboys, chancers and chaos. We're here to bring some unbiased clarity to a market.</h3>
+        <h3>Ready to get started?</h3>
+        </>
+    )
+}

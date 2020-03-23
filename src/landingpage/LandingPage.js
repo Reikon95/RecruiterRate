5996bda3a4b76dee2413e5812d6724e4a475f73d
Cameron Blackwood <cameronjwblackwood@gmail.com>
2020-03-23 21:10:50 +0000

    added navbar on landing page

--- src/landingpage/LandingPage.js
+++ src/landingpage/LandingPage.js
@@ -2,19 +2,29 @@ import React from 'react';
 import './LandingPage.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
-import {Link} from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
 export default function LandingPage() {
-    
+
     return (
         <div className="landingPageBody">
-        <h1>Say goodbye to smoke and mirrors.</h1>
-        <h1>Say hello to simple hiring!</h1>
-        <h3>RecruiterRate is a simple platform to discover and review recruiters. For too long, the industry has suffered from 
+            <Navbar bg="light" expand="lg" fixed="top" >
+                <Navbar.Brand href="#">Navbar</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+            </Navbar>
+            <h1>Say goodbye to smoke and mirrors.</h1>
+            <h1>Say hello to simple hiring!</h1>
+            <h3>RecruiterRate is a simple platform to discover and review recruiters. For too long, the industry has suffered from
         cowboys, chancers and chaos. We're here to bring some unbiased clarity to a market.</h3>
-        <h3>Ready to get started?</h3>
-        <Link to="/signup"><Button variant="danger">Sign Up</Button></Link>
-        <br/>
-        <Link to="/login">Already have an account? Log in</Link>
+            <h3>Ready to get started?</h3>
+            <Link to="/signup"><Button variant="danger">Sign Up</Button></Link>
+            <br />
+            <Link to="/login">Already have an account? Log in</Link>
         </div>
     )
 }

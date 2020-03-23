fe43aad36a5ae94c0454a497249f4c578dc35266
Cameron Blackwood <cameronjwblackwood@gmail.com>
2020-03-23 20:49:37 +0000

    added dependencies

--- src/landingpage/LandingPage.js
+++ src/landingpage/LandingPage.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import './LandingPage.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function LandingPage() {
+    
     return (
-        <>
+        <div className="landingPageBody">
         <h1>Say goodbye to smoke and mirrors.</h1>
         <h1>Say hello to simple hiring!</h1>
         <h3>RecruiterRate is a simple platform to discover and review recruiters. For too long, the industry has suffered from 
         cowboys, chancers and chaos. We're here to bring some unbiased clarity to a market.</h3>
         <h3>Ready to get started?</h3>
-        </>
+        
+        </div>
     )
 }

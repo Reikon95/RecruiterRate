a7eda9874926b24a7527feaf339708f7c67dc28b
Cameron Blackwood <cameronjwblackwood@gmail.com>
2020-03-24 17:14:06 +0000

    tabs

--- src/FAQ/FAQs.js
+++ src/FAQ/FAQs.js
@@ -1,33 +1,28 @@
 import React from 'react';
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
+import Sonnet from 'react-bootstrap/Sonnet';
 import Button from 'react-bootstrap/Button';
+import FAQCandidates from './FAQCandidates';
+import FAQEmployers from './FAQEmployers';
+import FAQRecruiters from './FAQRecruiters';
 export default function FAQs() {
     return (
         <>
         <h1>FAQs</h1>
-        <h2>Employers</h2>
-<Accordion defaultActiveKey="0">
-  <Card>
-    <Card.Header>
-      <Accordion.Toggle as={Button} variant="link" eventKey="0">
-        Click me!
-      </Accordion.Toggle>
-    </Card.Header>
-    <Accordion.Collapse eventKey="0">
-      <Card.Body>Hello! I'm the body</Card.Body>
-    </Accordion.Collapse>
-  </Card>
-  <Card>
-    <Card.Header>
-      <Accordion.Toggle as={Button} variant="link" eventKey="1">
-        Click me!
-      </Accordion.Toggle>
-    </Card.Header>
-    <Accordion.Collapse eventKey="1">
-      <Card.Body>Hello! I'm another body</Card.Body>
-    </Accordion.Collapse>
-  </Card>
-</Accordion>        </>
+        <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+  <Tab eventKey="home" title="Home">
+    <Sonnet />
+  </Tab>
+  <Tab eventKey="profile" title="Profile">
+    <Sonnet />
+  </Tab>
+  <Tab eventKey="contact" title="Contact" disabled>
+    <Sonnet />
+  </Tab>
+</Tabs>
+        </>
     )
 }

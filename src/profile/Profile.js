1c8783619bd62fef2d85615d1e7714512a469a18
Cameron Blackwood <cameronjwblackwood@gmail.com>
2020-03-24 19:27:52 +0000

    added a star rating system to the profile page

--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './Profile.css';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
+import StarRating from 'react-svg-star-rating'
+
 export default function Profile() {
     const imgurl = 'https://ih0.redbubble.net/image.909979035.9762/flat,750x,075,f-pad,750x1000,f8f8f8.jpg';
     const currentRating = '9.8/10';
@@ -9,19 +11,28 @@ export default function Profile() {
     const recruiterAgency = 'Meme Search LTD'
     return (
         <>
-        <h1>{recruiterName}</h1>
-        <h3>{recruiterAgency}</h3>
-    <h3>Current rating: {currentRating}</h3>
-        <img src={imgurl} alt="user" className="recruiterProfilePicture"/>
-        <Tabs defaultActiveKey="candidates" id="uncontrolled-tab-example">
-                <Tab eventKey="candidates" title="Candidates">
-                    Something
+            <h1>{recruiterName}</h1>
+            <h3>{recruiterAgency}</h3>
+            <h3>Current rating: {currentRating}</h3>
+            <img src={imgurl} alt="user" className="recruiterProfilePicture" />
+            <Tabs defaultActiveKey="Testimonials" id="uncontrolled-tab-example">
+                <Tab eventKey="Testimonials" title="Testimonials">
+                    "Mike helped me with a full stack developer job, great service" - Anon
                 </Tab>
-                <Tab eventKey="employers" title="Employers">
-                    Something
+                <Tab eventKey="Jobs" title="Jobs">
+                    {recruiterName} covers all software engineering roles in these locations:
                 </Tab>
-                <Tab eventKey="recruiters" title="Recruiters" >
-                    Something
+                <Tab eventKey="Contact" title="Contact" >
+                    Phone:
+                    Email:
+                    Linkedin:
+                    Twitter:
+                </Tab>
+                <Tab eventKey="Rate" title={`Rate ${recruiterName}`} >
+                <StarRating count="10"/>
+                Comments:
+                <textarea></textarea>
+                <input type="submit"/>
                 </Tab>
             </Tabs>
         </>

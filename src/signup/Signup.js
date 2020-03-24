7c78f9c4c303529d6ca0b942c1a55c1bbc004ede
Cameron Blackwood <cameronjwblackwood@gmail.com>
2020-03-24 16:46:49 +0000

    conditional rendering for signup

--- src/signup/Signup.js
+++ src/signup/Signup.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Signup.css';
 export default function SignUp() {
+    let [userType, setUserType] = useState(false);
     return (
         <>
-            <form>
-                <label>Email:</label>
-                <input type="email" />
-                <label>Confirm Email:</label>
-                <input type="email" />
-                <label>Password:</label>
-                <input type="password" />
-                <label>Confirm Password:</label>
-                <input type="password" />
-            </form>
+            Please describe yourself:
+            <label>Recruiter:</label>
+            <input type="radio" name="userType" onChange={() => setUserType("Recruiter")} />
+            <label>Jobseeker:</label>
+            <input type="radio" name="userType" onChange={() => setUserType("Jobseeker")} />
+            <label>Employer:</label>
+            <input type="radio" name="userType" onChange={() => setUserType("Employer")} />
+            <div className="signupGap">
+            {userType === "Recruiter" ?
+                <>
+                    Recruiter
+                </>
+                : userType === "Jobseeker" ?
+                    <>
+                        Jobseeker
+                    </>
+                    : userType === "Employer" ?
+                        <>
+                            Employer
+                        </>
+                        :
+                        <>
+                            Select a role above to contunue
+                        </>
+            }
+            </div>
         </>
     )
 }
